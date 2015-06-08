247b787bc2f4be5245c8bb4bb10841a681479ee0
wesley305 <wesley305@gmail.com>
2015-06-08 18:35:18 +0000

    wesley, dear I have made changes to index.php, kindly check

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        echo "Hello Wesley, I hope you are liking it !";
         ?>
     </body>
 </html>
